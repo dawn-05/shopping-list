bc6747717149556e57d000f425775cec0934f737
mimilili <dawnlin15@gmail.com>
2020-11-06 14:39:28 -0500

    fixed the check delete button with jquery

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ $(function(){
 
        $("ul").append(" <li> <span class=\"shopping-item\"> "+ name +" </span> <div class=\"shopping-item-controls\"> <button class=\"shopping-item-toggle\"> <span class=\"button-label\">check</span> </button> <button class=\"shopping-item-delete\"> <span class=\"button-label\">delete</span> </button> </div> </li>");
     });
-        $(".shopping-item-delete").on("click", function() {
+        $(".shopping-list").on("click", ".shopping-item-delete", function() {
             $(this).parent().parent().remove();
         });
 
-        $(".shopping-item-toggle").on("click", function(){
-            $(this).closest("li").find("span").toggleClass("shopping-item__checked");
+        $(".shopping-list").on("click", ".shopping-item-toggle",function(){
+            $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
         });
 });
